b9d644fa24bf37d78c61f6ee0174267b9c451b69
yonureker <yonureker@gmail.com>
2019-05-24 16:04:04 -0700

    Complete Hashing

--- Hash_map_LRU/skeleton/lib/p02_hashing.rb
+++ Hash_map_LRU/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    return 1.hash if self.empty?
+
+    (self[0..-2].sum * 2 + self[-1]).hash
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.each_char.with_index do |char, idx|
+      sum += (char.ord * idx)
+    end
+    sum.hash
   end
 end
 
@@ -16,6 +24,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each do |key, value|
+      k = (key.to_s.ord * 2)
+      v = (value.ord / 2)
+      sum += (k + v)
+    end
+    sum.hash
   end
 end
